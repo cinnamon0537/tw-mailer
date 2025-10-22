394ff6898bbfc0b2918e54233e2bbeda1dd45051
cinnamon0537 <benjamin.feichtlbauer@gmail.com>
2025-10-22 23:36:18 +0200

    add basic argument parsing client

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <getopt.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " <ip> <port>\n";
+        return 1;
+    }
+
+    std::string ip = argv[1];
+    int port = std::stoi(argv[2]);
+
+    std::cout << "Starting client at " << port << " for " << ip << "\n";
+
     return 0;
 }
