3c284713cefa58a454bac9ae6dad0b5333545bac
cinnamon0537 <benjamin.feichtlbauer@gmail.com>
2025-10-22 22:52:33 +0200

    add basic argument parsing

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <getopt.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " <port> <mail-spool-directoryname>\n";
+        return 1;
+    }
+
+    int port = std::stoi(argv[1]);
+    std::string msg = argv[2];
+
+    std::cout << "Starting server at " << port << " for " << msg << "\n";
+
     return 0;
 }
